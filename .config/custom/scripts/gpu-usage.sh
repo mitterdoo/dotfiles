7cb2d5b5bb612046e7b5dbcdbc229077c84c524c
Connor Ashcroft <connorashcroft@live.com>
2026-02-01 20:29:59 -0800

    AMD gpu

--- .config/custom/scripts/gpu-usage.sh
+++ .config/custom/scripts/gpu-usage.sh
@@ -1,16 +1,12 @@
-a=$(nvidia-smi -i 0 --query-gpu=power.draw --format=csv,noheader,nounits);
-b=$(nvidia-smi -i 0 --query-gpu=power.limit --format=csv,noheader,nounits);
-a=${a/.*};
-b=${b/.*};
-usage=$((a*100/b));
+usage=$(amd-smi metric -u | grep -oP 'GFX_ACTIVITY: \K\d+' -m 1);
 
 # in MiB
-mem_used=$(nvidia-smi -i 0 --query-gpu=memory.used --format=csv,noheader,nounits);
-mem_total=$(nvidia-smi -i 0 --query-gpu=memory.total --format=csv,noheader,nounits);
+mem_used=$(amd-smi metric -m | grep -oP 'USED_VRAM: \K\d+');
+mem_total=$(amd-smi metric -m | grep -oP 'TOTAL_VRAM: \K\d+');
 
 mem_perc=$((mem_used*100/mem_total));
 
-temp=$(nvidia-smi -i 0 --query-gpu=temperature.gpu --format=csv,noheader,nounits);
+temp=$(amd-smi metric -t | grep -oP 'MEM: \K\d+');
 
 echo "{\"text\": \"GPU <span color='#00FF00'>${usage}</span><span color='#00FFFF'>%</span> / <span color='#00FF00'>${temp}</span><span color='#00FFFF'>°</span>   VRAM <span color='#00FF00'>${mem_perc}</span><span color='#00FFFF'>%</span> / <span color='#00FF00'>${mem_used}</span> <span color='#00FFFF'>MiB</span>\"}"
 
