55f3645b63513a2226b108230a5a5fc20f7cdb7b
Connor Ashcroft <connorashcroft@live.com>
2026-02-03 13:46:13 -0800

    gpu usage part 2

--- .config/custom/scripts/gpu-usage.sh
+++ .config/custom/scripts/gpu-usage.sh
@@ -1,3 +1,11 @@
+# iterations=1;
+
+# usage_sum=0;
+# for (( i=1; i<=iterations; i++ )) do
+# 	this_usage=$(amd-smi metric -u | grep -oP 'GFX_ACTIVITY: \K\d+' -m 1);
+# 	usage_sum=$((usage_sum+this_usage));
+# done
+# usage=$((usage_sum/iterations));
 usage=$(amd-smi metric -u | grep -oP 'GFX_ACTIVITY: \K\d+' -m 1);
 
 # in MiB
@@ -8,5 +16,36 @@ mem_perc=$((mem_used*100/mem_total));
 
 temp=$(amd-smi metric -t | grep -oP 'MEM: \K\d+');
 
-echo "{\"text\": \"GPU <span color='#00FF00'>${usage}</span><span color='#00FFFF'>%</span> / <span color='#00FF00'>${temp}</span><span color='#00FFFF'>°</span>   VRAM <span color='#00FF00'>${mem_perc}</span><span color='#00FFFF'>%</span> / <span color='#00FF00'>${mem_used}</span> <span color='#00FFFF'>MiB</span>\"}"
+perc_color='00FF00';
+perc_decorator_start='';
+perc_decorator_end='';
+if (( usage >= 99 )); then
+	perc_color='00FFFF';
+	perc_decorator_start='<b>';
+	perc_decorator_end='</b>';
+fi
+
+temp_color='00FF00';
+temp_decorator_start='';
+temp_decorator_end='';
+if (( temp >= 90 )); then
+	temp_color='FF7F00';
+	temp_decorator_start='<b>';
+	temp_decorator_end='</b>';
+fi
+if (( temp >= 102 )); then
+	temp_color='FF0000';
+fi
+
+mem_color='00FF00';
+mem_decorator_start='';
+mem_decorator_end='';
+if (( mem_perc >= 95 )); then
+	mem_color='FF7F00';
+	mem_decorator_start='<b>';
+	mem_decorator_end='</b>';
+fi
+usage=$(printf '%2d' $usage);
+mem_perc=$(printf '%2d' $mem_perc);
+echo "{\"text\": \"GPU ${perc_decorator_start}<span color='#${perc_color}'>${usage}</span><span color='#00AAFF'>%</span>${perc_decorator_end} / ${temp_decorator_start}<span color='#${temp_color}'>${temp}</span><span color='#00AAFF'>°</span>${temp_decorator_end}   VRAM ${mem_decorator_start}<span color='#${mem_color}'>${mem_perc}</span><span color='#00AAFF'>%</span>${mem_decorator_end} / ${mem_decorator_start}<span color='#${mem_color}'>${mem_used}</span> <span color='#00AAFF'>MiB</span>${mem_decorator_end}\"}"
 
